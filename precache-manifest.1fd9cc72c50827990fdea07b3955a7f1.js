39d09886bfb4f6d45fb202bce5a7319265cf4689
ckosinski <37275817+cjk-web@users.noreply.github.com>
2019-12-31 08:30:24 -0500

    Updates

---
+++ precache-manifest.1fd9cc72c50827990fdea07b3955a7f1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36efa6495cbb66e90a47650f3f33d067",
+    "url": "/react-activity-tracker/index.html"
+  },
+  {
+    "revision": "556cb41b0c679f0fa5b3",
+    "url": "/react-activity-tracker/static/css/main.5ae13fd0.chunk.css"
+  },
+  {
+    "revision": "d92d2d796c274d6ca12f",
+    "url": "/react-activity-tracker/static/js/2.825e76fe.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-activity-tracker/static/js/2.825e76fe.chunk.js.LICENSE"
+  },
+  {
+    "revision": "556cb41b0c679f0fa5b3",
+    "url": "/react-activity-tracker/static/js/main.c4d180cb.chunk.js"
+  },
+  {
+    "revision": "f472b28ebe57c8c61038",
+    "url": "/react-activity-tracker/static/js/runtime-main.31473dfe.js"
+  }
+]);
